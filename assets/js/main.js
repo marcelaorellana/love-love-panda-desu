358ffae540820458ad73b553d86b7deeb82f5995
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-18 21:59:54 -0400

    desarrollado el ejercicio enunciado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+var fotos = document.getElementById("fotosId");
+pictures.forEach(function(valor){
+	fotos.innerHTML += "<img class='fotitos' src=" + valor + ">";
+});
